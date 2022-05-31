cd8c69099c1327a89053d9ecb0d91a2110b6b51b
mason park <joung4342@gmail.com>
2022-05-31 21:24:27 +0900

    setting firebase

--- src/index.js
+++ src/index.js
@@ -2,10 +2,24 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.module.css';
 import App from './app';
+import { initializeApp } from "firebase/app";
+import { getFirestore, collection, getDocs } from 'firebase/firestore/lite';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+const firebaseConfig ={
+    apiKey: process.env.REACT_APP_FIREBASE_APIKEY,
+    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+    databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_FIREBASE_APIKEY_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_FIREBASE_APPID,
+    measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
+}
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
 root.render(
   <React.StrictMode>
-    <App />
+    <App firebaseApp={app} firebaseDB={db}/>
   </React.StrictMode>
 );
