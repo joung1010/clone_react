f17c24092b6f2e039c34cb34ea817c28e4a5ccb0
mason park <joung4342@gmail.com>
2022-06-06 20:17:01 +0900

    add router

--- src/app.jsx
+++ src/app.jsx
@@ -1,9 +1,12 @@
 import styles from './app.module.css'
 import Login from "./components/login/login";
 import {useState} from "react";
+import {useNavigate} from "react-router-dom";
 
 function App({authService}) {
   const [user,setUser] = useState(null);
+
+  const navigate = useNavigate();
 
   const login = (type) => {
       authService.login(type)
